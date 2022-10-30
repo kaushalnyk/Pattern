91bf770c5694773590786c4029b209d82d7e1021
kaushalnyk <nayak.kausal12@gmail.com>
2022-10-30 17:51:36 +0530

    Star

---
+++ StarThree.java
@@ -0,0 +1,21 @@
+package in.ineuron.main;
+
+public class StarThree {
+
+	public static void main(String[] args) {
+		int n=11;
+		for(int i=0; i<n; i++) {
+		    for(int j=0; j<n; j++) {
+		        if(i+j<=(n-1)/2 ||i-j>=(n-1)/2 ||i==0 ||i==n-1)
+		        {
+		            System.out.print("* ");
+		        }else {
+		            System.out.print("  ");
+		        }
+		    }
+		    System.out.println();
+		}
+
+	}
+
+}
